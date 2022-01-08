7570e3b1f338114d011a1db15aa68d97621d64db
Daniel Matuszewski <39099569+strus100@users.noreply.github.com>
2022-01-08 09:44:21 +0100

    Initial commit

--- src/main/scala/org/example/SparkSessionTest.scala
+++ src/main/scala/org/example/SparkSessionTest.scala
@@ -2,29 +2,23 @@ package org.example
 
 import org.apache.spark.sql.SparkSession
 
+import scala.collection.mutable.ListBuffer
+
 object SparkSessionTest {
 
-  def main(args:Array[String]): Unit ={
+  def main(args: Array[String]): Unit = {
 
-    val spark = SparkSession.builder()
-      .master("local[1]")
-      .appName("SparkByExample")
-      .getOrCreate();
-    
-    println("First SparkContext:")
-    println("APP Name :"+spark.sparkContext.appName);
-    println("Deploy Mode :"+spark.sparkContext.deployMode);
-    println("Master :"+spark.sparkContext.master);
+    System.setProperty("hadoop.home.dir", "C:\\Hadoop")
+    System.setProperty("spark.home.dir", "C:\\Spark")
+    val spark = SparkSession.builder().master("local")
+      .getOrCreate()
 
-    val sparkSession2 = SparkSession.builder()
-      .master("local[1]")
-      .appName("SparkByExample-test")
-      .getOrCreate();
-
-    println("Second SparkContext:")
-    println("APP Name :"+sparkSession2.sparkContext.appName);
-    println("Deploy Mode :"+sparkSession2.sparkContext.deployMode);
-    println("Master :"+sparkSession2.sparkContext.master);
-
+    val tab_source = new ListBuffer[(Int, Int, Int)]()
+    for (a <- (1 to 500); b <- (1 to 200); c <- (1 to 50))
+      tab_source += ((a,b,c))
+    val tab_source_list = tab_source.toList
+    val rdd = spark.sparkContext.parallelize(tab_source_list)
+    println( rdd.count() )
   }
+
 }
